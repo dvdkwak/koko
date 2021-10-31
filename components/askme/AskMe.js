1ce0d45ac3efc78718c5635d6fda7b5500ca926c
dvdkwak <dvdkwak@gmail.com>
2021-10-31 14:15:34 +0100

    [Koko v2]: no clue what changed here tbh...

--- components/askme/AskMe.js
+++ components/askme/AskMe.js
@@ -1,56 +1,56 @@
 class AskMe extends React.Component {
 
 
-    /**
-     * setting the socket and default value
-     * @param {Object} props props form react
-     */
-    constructor(props) {
-        super(props);
-        this.state =  {
-            value: ""
-        }
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+  /**
+   * setting the socket and default value
+   * @param {Object} props props form react
+   */
+  constructor(props) {
+    super(props);
+    this.state =  {
+        value: ""
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
 
-    /**
-     * Emitting the command to the socket server and emptying the input field
-     * (by use of the state)
-     * @param {*} event event of the form
-     */
-    handleSubmit(event) {
-        console.log(`Your command: ${this.state.value}`);
-        this.props.socket.emit('command', this.state.value);
-        this.setState({value: ''});
-        event.preventDefault();
-    } // end of handleSubmit
+  /**
+   * Emitting the command to the socket server and emptying the input field
+   * (by use of the state)
+   * @param {*} event event of the form
+   */
+  handleSubmit(event) {
+    console.log(`Your command: ${this.state.value}`);
+    this.props.socket.emit('command', this.state.value);
+    this.setState({value: ''});
+    event.preventDefault();
+  } // end of handleSubmit
 
 
-    /**
-     * handeling what to do on typing within the input field
-     * @param {*} event event of the input field
-     */
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    } // end of handleChange
+  /**
+   * handeling what to do on typing within the input field
+   * @param {*} event event of the input field
+   */
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  } // end of handleChange
 
 
-    /**
-     * Rendering item to the DOM
-     */
-    render() {
-        return(
-            <form id="AskMeForm" onSubmit={this.handleSubmit}>
-            <input 
-                style={this.style} 
-                type="text" 
-                placeholder="Ask me anything!"
-                value={this.state.value}
-                onChange={this.handleChange} />
-            </form>
-        );
-    } // end of render
+  /**
+   * Rendering item to the DOM
+   */
+  render() {
+    return(
+      <form id="AskMeForm" onSubmit={this.handleSubmit}>
+      <input 
+        style={this.style} 
+        type="text" 
+        placeholder="Ask me anything!"
+        value={this.state.value}
+        onChange={this.handleChange} />
+      </form>
+    );
+  } // end of render
 
 }
